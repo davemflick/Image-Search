ab11b0c893965c5a37bc97e0e2b0d67a18da09a7
davemflick <davemflick@yahoo.com>
2017-07-03 08:47:15 -0400

    re-structured, starting to build

--- app.js
+++ app.js
@@ -1,36 +1,19 @@
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
+var mongoose = require('mongoose');
+
 
 var app = express();
-
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get("/", function(req, res){
+	res.render("home")
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(process.env.PORT || 3000, process.env.IP, ()=>{
+	console.log("Image Search Abstraction Running");
 });
-
-module.exports = app;
